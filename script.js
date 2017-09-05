2fa73f6b4d036f69764f56d83197b1dc60120d96
rcmorrill <rcmorrill@gmail.com>
2017-09-05 17:07:15 -0400

    figuring out data structures

--- script.js
+++ script.js
@@ -15,56 +15,58 @@ var plot = d3.select('.canvas')
 
 
 
+
+
+var scaleY = d3.scale.linear().domain([1,11]).range([1,500]);
+
+
+d3.csv('data/fleet_foxes_V1_alt.csv',parse,dataLoaded);
+
+
+
 //PRIMARY FUNCTION
-
-
-
-
-d3.csv('data/ff.csv',parse,dataLoaded);
-
-
-
-
 
 function dataLoaded(err,data){
 
+var nestedData = d3.nest()
+	.key(function(d) {return d.artist})
+	.entries(data);
+
+console.log(nestedData);
 
 
-// plot.selectAll('.lines')
-// 	.data(data)
-// 	.enter()
-// 	.append('rect').attr('class','lines')
-// 	.attr('x',function(d){return d.ccode})
-// 	.attr('y', function(d){return scaleYLines(d.startDate)})
-// 	//.attr('r',10)
-// 	.attr('height',function(d){ 
-// 		var length = scaleYLines(d.endDate) - scaleYLines(d.startDate);
-// 		return length;
-// 	})
-// 	.attr('width', 3)
-// 	//.attr('fill',function(d){return scaleC(d.location)})
-// 	.attr('fill','rgba(12,12,12,.1)')
+var plotting = plot.selectAll('.circles')
+	.data(nestedData[3].values)
+	.enter()
+	//.append('text').text('fake')
+	.append('g')
 
 
+plotting.append('circle').attr('class','.circles')
+	.attr('cx', function(d) {return d.length})//function(d){return d.length})
+	.attr('cy', function(d) {return scaleY(d.track)})//function(d){return scaleYLines(d.startDate)})
+	.attr('r',function(d){return 30})
+	.style('fill','rgba(45,45,45,.3)')
 
-}
+plotting.append('text').attr('class','.labels')
+	.attr('x', function(d) {return d.length})
+	.attr('y', function(d) {return scaleY(d.track)})
+	.text(function(d){return d.instrument})
+
+
+}//END DATA LOADED
 
 
 function parse(d){
 
 
 	return{
-		country: d.StateName,
-		ccode: +d.ccode,
-		initiator: d.Initiator,
-		length: d.Length,
-		war: d.WarName,
-		start: +d.StartYear1,
-		casualties: +d.BatDeath,
-		outcome: d.Outcome,
-		location: d.WhereFought,
-		startDate: new Date(+d.StartYear1, + d.StartMonth1-1, + d.StartDay1),
-		endDate: new Date(+d.EndYear1, + d.EndMonth1-1, + d.EndDay1),
+		track: d.track,
+		name: d.Name,
+		length: +d.length,
+		artist: d.artist,
+		primary: d.primary,
+		instrument: d.instrument,
 	}
 }
 
